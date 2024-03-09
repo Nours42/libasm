194e5bc1ed517cfd27ad4f2d900b3ab9466bfd79
Nours42 <destannes.sebastien@gmail.com>
2024-03-09 15:25:00 +0100

    ft_strdup et ft_strcpy

---
+++ ft_strcpy/ft_strcpy.s
@@ -0,0 +1,20 @@
+section	.text
+	global	_ft_strcpy
+
+_ft_strcpy:							; dst = rdi, src = rsi
+	xor		rcx, rcx				; i = 0
+	xor		rdx, rdx				; tmp = 0
+	cmp		rsi, 0					; !rsi
+	jz		return                  ; jz = jump if zero (if rsi is null)
+	jmp		copy
+increment:
+	inc		rcx
+copy:
+	mov		dl, BYTE [rsi + rcx]    ; Copie le caractère de src à dst. Le caractère
+	mov		BYTE [rdi + rcx], dl    ; à l'index rcx dans src (rsi + rcx) est chargé
+	cmp		dl, 0                   ; dans dl (partie basse de rdx) puis stocké à
+	jnz		increment               ; l'index rcx dans dst (rdi + rcx). puis compare
+                                    ; si dl = 0 (fin de la copie) et jnz remonte a increment
+return:
+	mov		rax, rdi				; moove rdi sur rax (=valeur de retour) et donc return dst
+	ret
